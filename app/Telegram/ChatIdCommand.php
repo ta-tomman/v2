96a3e173c414cde32eb9a3598a529cbff53339e2
Khucink Ithem <meongx@gmail.com>
2016-12-31 22:46:27 +0800

    refactor common telegram api parameters into CommandBase

--- app/Telegram/ChatIdCommand.php
+++ app/Telegram/ChatIdCommand.php
@@ -2,23 +2,18 @@
 
 namespace App\Telegram;
 
-use Telegram\Bot\Commands\Command;
-
-class ChatIdCommand extends Command
+class ChatIdCommand extends CommandBase
 {
     protected $name = 'chatid';
     protected $description = 'Nomor ID dari Chat yang aktif';
 
     public function handle($param)
     {
-        $msg = $this->getUpdate()->getMessage();
-        $id = $msg->getChat()->getId();
-        $replyId = $msg->getMessageId();
+        $id = $this->getUpdate()->getMessage()->getChat()->getId();
 
         $this->replyWithMessage([
             'text' => "ChatID: `$id`",
-            'parse_mode' => 'Markdown',
-            'reply_to_message_id' => $replyId
+            'parse_mode' => 'Markdown'
         ]);
     }
 }
---
+++ app/Telegram/CommandBase.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Telegram;
+
+use Telegram\Bot\Commands\Command;
+
+abstract class CommandBase extends Command
+{
+    public function replyWithMessage($param)
+    {
+        if (!isset($param['reply_to_message_id'])) {
+            $replyId = $this->getUpdate()->getMessage()->getMessageId();
+            $param['reply_to_message_id'] = $replyId;
+        }
+
+        parent::replyWithMessage($param);
+    }
+}
