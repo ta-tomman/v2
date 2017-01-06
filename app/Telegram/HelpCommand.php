9b2052b8343c61168275349118292c28aa25331b
Khucink Ithem <meongx@gmail.com>
2017-01-06 15:37:45 +0800

    init beagle template

---
+++ app/Telegram/HelpCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Telegram;
+
+use Telegram\Bot\Commands\Command;
+
+class HelpCommand extends Command
+{
+    protected $name = 'help';
+    protected $description = 'Help system';
+
+    public function handle($param)
+    {}
+
+    protected function handleUnknownCommand($param)
+    {
+        // TODO: normalize double dot special delimeter (..)
+    }
+
+    protected function showAvailableCommand()
+    {
+        // TODO: ignore 'debug'
+    }
+}
