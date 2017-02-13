db248609b78ac7e891825545ae7682eb7d176a4d
Khucink Ithem <meongx@gmail.com>
2017-02-13 13:20:23 +0800

    update documentation

--- app/Service/WebScreenShot.php
+++ app/Service/WebScreenShot.php
@@ -7,8 +7,8 @@ class WebScreenShot
     /**
      * Rasterize existing HTML file and write the output into file
      *
-     * @param filepath $srcPath input html
-     * @param filepath $dstPath output path with extension (pdf or png)
+     * @param string $srcPath path to input html file
+     * @param string $dstPath output path with extension (pdf or png)
      * @param string $dimension output dimension (image or paper size), default is '720px*1280px'
      */
     public static function rasterizeFile($srcPath, $dstPath, $dimension = '720px*1280px')
