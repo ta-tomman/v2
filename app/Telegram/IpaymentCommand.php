d0f111051858f9a5012c4da1515ddc0175f861ce
Khucink Ithem <meongx@gmail.com>
2017-02-13 13:20:37 +0800

    cache result

--- app/Telegram/IpaymentCommand.php
+++ app/Telegram/IpaymentCommand.php
@@ -13,21 +13,31 @@ class IpaymentCommand extends CommandBase
     public function handle($param)
     {
         try {
+            $jastel      = trim($param);
+            $outDir      = storage_path("telegram/ipayment");
+            $htmlOutPath = "{$outDir}/{$jastel}.html";
+            $imgOutPath  = "{$outDir}/{$jastel}.png";
+
+            if (file_exists($imgOutPath)) {
+                $lastModified = filemtime($imgOutPath);
+                $age          = time() - $lastModified;
+
+                // consider valid for 1 hour
+                if ($age < (60 * 60)) {
+                    $this->replyWithPhoto([
+                        'photo' => $imgOutPath
+                    ]);
+                    return;
+                }
+            }
+
             $this->replyWithChatAction([
                 'action' => 'upload_photo'
             ]);
 
-            $jastel = trim($param);
             $result = Ipayment::request($jastel);
-
-            // TODO: cache result view
-
-            $view = view('ipayment.view-bot', ['data' => $result]);
-            $html = $view->render();
-
-            $outDir      = storage_path("telegram/ipayment");
-            $htmlOutPath = "{$outDir}/{$jastel}.html";
-            $imgOutPath  = "{$outDir}/{$jastel}.png";
+            $view   = view('ipayment.view-bot', ['data' => $result]);
+            $html   = $view->render();
 
             @mkdir($outDir, 0755, true);
             file_put_contents($htmlOutPath, $html);
