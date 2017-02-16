9083e175fb85a76c5b9b1ce6b09b7b560b6c900f
Khucink Ithem <meongx@gmail.com>
2017-02-16 15:12:34 +0800

    custom storage directory

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // set custom storage directory
+        $path = env('PATH_STORAGE', false);
+        if ($path) {
+            if (!file_exists($path)) {
+                // make sure the path have necessary structure
+                $origin = realpath(__DIR__ . '/../..').DIRECTORY_SEPARATOR.'storage';
+                \File::copyDirectory($origin, $path);
+            }
+
+            $this->app->useStoragePath($path);
+        }
     }
 
     /**
