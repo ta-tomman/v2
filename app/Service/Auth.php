2d24152b05ce518f3cfc987655e96985afb1514b
Khucink Ithem <meongx@gmail.com>
2017-03-09 14:44:41 +0800

    permission system with test

---
+++ app/Service/Auth.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+class Auth
+{
+    const PERM = [
+        'NONE'          => 0b00000000,
+        'READ_OWN'      => 0b00000001,
+        'READ_GROUP'    => 0b00000010,
+        'READ_ALL'      => 0b00000111,
+        'WRITE_OWN'     => 0b00001000,
+        'WRITE_GROUP'   => 0b00010000,
+        'WRITE_ALL'     => 0b00011000,
+        'FULL'          => 0b11111111
+    ];
+
+
+    public static function hasPermission(string $module, string $permission, array $available): bool
+    {
+        foreach ($available as $mod => $perm) {
+            if (fnmatch($mod, $module) && ($permission & $perm))
+                return true;
+        }
+
+        return false;
+    }
+}
---
+++ tests/Unit/ServiceAuthTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App\Service\Auth;
+
+class ServiceAuthTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testHasPermissionExact()
+    {
+        $this->assertTrue(
+            Auth::hasPermission(
+                'auth.user',
+                Auth::PERM['READ_ALL'],
+                ['auth.user' => Auth::PERM['READ_ALL']]
+            )
+        );
+    }
+
+    public function testHasPermissionWildcard()
+    {
+        $this->assertTrue(
+            Auth::hasPermission(
+                'auth.user',
+                AUTH::PERM['READ_ALL'],
+                ['auth.*' => Auth::PERM['READ_ALL']]
+            )
+        );
+    }
+
+    public function testHasPermissionHigherPermission()
+    {
+        $this->assertTrue(
+            Auth::hasPermission(
+                'auth.user',
+                AUTH::PERM['READ_OWN'],
+                ['auth.*' => Auth::PERM['READ_ALL']]
+            )
+        );
+    }
+
+    public function testHasPermissionHigherModule()
+    {
+        $this->assertTrue(
+            Auth::hasPermission(
+                'auth.user.update',
+                AUTH::PERM['WRITE_OWN'],
+                ['auth.*' => Auth::PERM['WRITE_ALL']]
+            )
+        );
+    }
+
+    public function testHasPermissionMultipleAvailable()
+    {
+        $this->assertTrue(
+            Auth::hasPermission(
+                'hr.list',
+                AUTH::PERM['READ_ALL'],
+                [
+                    'auth.*' => Auth::PERM['WRITE_ALL'],
+                    'hr.*' => Auth::PERM['READ_ALL']
+                ]
+            )
+        );
+    }
+}
