edbdfacc52c8f238f1398a4748cf02ccb3bf2c1f
Khucink Ithem <meongx@gmail.com>
2017-03-10 14:37:13 +0800

    terminate connection early to prevent telegram queue blocking

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,20 @@
 */
 
 /** Telegram WebHook **/
-Route::post('hook/'.env('TELEGRAM_BOT_TOKEN'), function() { Telegram::commandsHandler(true); });
+Route::post('hook/'.env('TELEGRAM_BOT_TOKEN'), function() {
+    // terminate connection early
+    // so a command handler doesn't block telegram queue
+    header('Connection: close');
+    ob_start();
+    echo 'OK';
+    $size = ob_get_length();
+    header("Content-Length: $size");
+    ob_end_flush();
+    flush();
+
+    // proceeed with Telegram Command handler
+    Telegram::commandsHandler(true);
+});
 
 Route::group(['prefix' => 'tgram'], function() {
     Route::get('ipayment/{jastel}', 'Billing\IpaymentController@show');
