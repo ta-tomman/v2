e2ecfa9c35adede141f726b8434934de495bb91d
Khucink Ithem <meongx@gmail.com>
2017-03-24 09:54:35 +0800

    fix migration error

--- database/migrations/2017_03_23_023213_workzone.php
+++ database/migrations/2017_03_23_023213_workzone.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Workgroup extends Migration
+class Workzone extends Migration
 {
     /**
      * Run the migrations.
